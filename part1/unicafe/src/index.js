baa612844e9054db6d707b20a20275a566142cc9
Anthony Truong <gonnahurttony@gmail.com>
2020-05-05 23:36:50 -0700

    1.10

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -9,14 +9,11 @@ const Button = ({ handleClick, text }) => (
   </button>
 )
 
-const Statistics = ({ counter, text }) => (
+const Statistic = ({ counter, text }) => (
   <div>{text} {counter}</div>
 )
 
-const App = () => {
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+const Statistics = ({ good, neutral, bad }) => {
   const average = () => {
     if (good + neutral + bad === 0) {
       return 0
@@ -33,6 +30,26 @@ const App = () => {
     }
   }
 
+  if (good + neutral + bad === 0) {
+    return <p>No feedback given</p>
+  } else {
+    return (
+      <div>
+        <Statistic counter={good} text='good' />
+        <Statistic counter={neutral} text='neutral' />
+        <Statistic counter={bad} text='bad' />
+        <Statistic counter={good + neutral + bad} text='all' />
+        <Statistic counter={average()} text='average' />
+        <Statistic counter={positivePercentage()} text='positive' />
+      </div>)
+  }
+}
+
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
   return (
     <div>
       <Header text='give feedback' />
@@ -40,12 +57,7 @@ const App = () => {
       <Button handleClick={() => setNeutral(neutral + 1)} text='neutral' />
       <Button handleClick={() => setBad(bad + 1)} text='bad' />
       <Header text='statistics' />
-      <Statistics counter={good} text='good' />
-      <Statistics counter={neutral} text='neutral' />
-      <Statistics counter={bad} text='bad' />
-      <Statistics counter={good + neutral + bad} text='all' />
-      <Statistics counter={average()} text='average' />
-      <Statistics counter={positivePercentage()} text='positive' />
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
