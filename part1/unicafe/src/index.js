1588806ba1547e5ed1772d0f0bf1aed6ee6044bc
Anthony Truong <gonnahurttony@gmail.com>
2020-05-05 23:13:14 -0700

    1.6

---
+++ part1/unicafe/src/index.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+
+const Header = ({ text }) => (<h1>{text}</h1>)
+
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
+
+const Statistics = ({ counter, text }) => (
+  <div>{text} {counter}</div>
+)
+
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>
+      <Header text='give feedback' />
+      <Button handleClick={() => setGood(good + 1)} text='good' />
+      <Button handleClick={() => setNeutral(neutral + 1)} text='neutral' />
+      <Button handleClick={() => setBad(bad + 1)} text='bad' />
+      <Header text='statistics' />
+      <Statistics counter={good} text='good' />
+      <Statistics counter={neutral} text='neutral' />
+      <Statistics counter={bad} text='bad' />
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
