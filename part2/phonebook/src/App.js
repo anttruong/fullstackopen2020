2e91bc873ee18b2e381577ffc8a8aae3f057a707
Anthony Truong <gonnahurttony@gmail.com>
2020-05-10 22:05:56 -0700

    part 2.7

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -12,8 +12,12 @@ const App = () => {
   const addName = event => {
     event.preventDefault()
     const newPerson = { name: newName }
-    setPersons(persons.concat(newPerson))
-    setNewName('')
+    if (persons.map(person => person.name).includes(newPerson.name)) {
+      alert(`${newName} is already added to phonebook`)
+    } else {
+      setPersons(persons.concat(newPerson))
+      setNewName('')
+    }
   }
 
   const handleNameChange = event => {
