7b4272475d593cb35714bc36e2b3126082188a6f
Anthony Truong <gonnahurttony@gmail.com>
2020-05-10 22:59:08 -0700

    2.11

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Persons = ({ phonebook, search }) => (
   phonebook
@@ -50,14 +51,15 @@ const Filter = ({ newSearch, setNewSearch }) => {
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-1234567' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
-
+  const [persons, setPersons] = useState([])
   const [newSearch, setNewSearch] = useState('')
+
+  const url = 'http://localhost:3001/persons'
+  useEffect(() => {
+    axios.get(url).then(response => {
+      setPersons(response.data)
+    })
+  }, [])
 
   return (
     <div>
