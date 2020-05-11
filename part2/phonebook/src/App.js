44a417ec7e6bba08ff75ef2d62ea3759fb0fc2c9
Anthony Truong <gonnahurttony@gmail.com>
2020-05-10 22:37:00 -0700

    part 2.10

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,20 +1,13 @@
 import React, { useState } from 'react'
 
-const Display = ({ phonebook, search }) => (
+const Persons = ({ phonebook, search }) => (
   phonebook
     .filter(person => person.name.toLowerCase().includes(search.toLowerCase()))
     .map(person => (<div key={person.name}>{person.name} {person.number}</div>)))
 
-const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-1234567' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
+const PersonForm = ({ persons, setPersons }) => {
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
-  const [newSearch, setNewSearch] = useState('')
 
   const addName = event => {
     event.preventDefault()
@@ -36,26 +29,44 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
+  return (
+    <form onSubmit={addName}>
+      <div>name: <input value={newName} onChange={handleNameChange} /></div>
+      <div>number: <input value={newNumber} onChange={handleNumberChange} /></div>
+      <div><button type="submit">add</button></div>
+      <div>
+        debug: {newName}
+      </div>
+    </form>)
+}
+
+const Filter = ({ newSearch, setNewSearch }) => {
   const handleSearchChange = event => {
     setNewSearch(event.target.value)
   }
 
+  return (<div>filter shown with <input value={newSearch}
+    onChange={handleSearchChange} /></div>)
+}
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-1234567' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+  ])
+
+  const [newSearch, setNewSearch] = useState('')
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <div>filter shown with <input value={newSearch}
-        onChange={handleSearchChange} /></div>
-      <h2>add a new</h2>
-      <form onSubmit={addName}>
-        <div>name: <input value={newName} onChange={handleNameChange} /></div>
-        <div>number: <input value={newNumber} onChange={handleNumberChange} /></div>
-        <div><button type="submit">add</button></div>
-        <div>
-          debug: {newName}
-        </div>
-      </form>
-      <h2>Numbers</h2>
-      <Display phonebook={persons} search={newSearch}/>
+      <Filter newSearch={newSearch} setNewSearch={setNewSearch} />
+      <h3>add a new</h3>
+      <PersonForm persons={persons} setPersons={setPersons} />
+      <h3>Numbers</h3>
+      <Persons phonebook={persons} search={newSearch} />
     </div>
   )
 }
